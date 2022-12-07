090aea708139ecd935ec25686ced17b9122b04d5
Luleka Mgaga <lulekamgaga@hotmail.com>
2022-12-07 16:50:55 +0000

    exercise completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
 import PokemonCity from "./PokemonCity";
 import PokemonMovesSelector from "./PokemonMovesSelector";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 
 function App() {
@@ -11,14 +12,32 @@ function App() {
     console.log("Hello");
   }
   return (
+    <BrowserRouter>
+     <ul>
+      <li>
+        <Link to="/Logo">Logo Page</Link>
+        </li>
+        <li><Link to="/BestPokemon">Pokemon Page</Link>
+        </li>
+        <li><Link to="CaughtPokemon">CaughtPokemon Page</Link>
+        </li>
+        <li><Link to="PokemonMovesSelector">PokemonMovesSelector </Link>
+        </li>
+     </ul>
    <div>
       <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
       <PokemonMovesSelector />
       <PokemonCity />
+      <Routes>
+        <Route path="/BestPokemon" element={<BestPokemon />} />
+        <Route path="/CaughtPokemon" element={<CaughtPokemon/>} />
+      </Routes>
+     
    </div>
-  
+    </BrowserRouter>
+    
     );
 
 }
